df932bfe8a47e200faabc93b4224f2293675aa3c
Alwandia Ridwan Wadiska <alwandiarw@gmail.com>
2024-06-01 13:19:12 +0700

    feat: add Part-2b - filtering displayed elements

--- Part-2/apart2-intro/intro/src/App.jsx
+++ Part-2/apart2-intro/intro/src/App.jsx
@@ -6,6 +6,7 @@ const App = (props) => {
   // const { notes } = props;
   const [notes, setNotes] = useState(props.notes);
   const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
 
   const addNote = (event) => {
     event.preventDefault();
@@ -25,13 +26,24 @@ const App = (props) => {
     setNewNote(event.target.value);
   };
 
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important); // note.important === true
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
+
+        {/* {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))} */}
 
         {/* //! if using destructuring props */}
         {/* {notes.map((note) => (
