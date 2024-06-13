b5ff8f28b39c13fa755cf10daf2174e88aff2413
Alwandia Ridwan Wadiska <alwandiarw@gmail.com>
2024-06-13 16:19:12 +0700

    feat: added Part-2c - The development runtime environment

--- Part-2/apart2-intro/2C-GettingDataFromServer/src/App.jsx
+++ Part-2/apart2-intro/2C-GettingDataFromServer/src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Note from "./components/Note";
 
-const App = (props) => {
+const App = () => {
+  //!removing props
   //{notes}
   // const { notes } = props;
-  const [notes, setNotes] = useState(props.notes);
+  const [notes, setNotes] = useState([]); // props.notes
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+
+  // useEffect(() => {
+  //   console.log("effect");
+  //   axios
+  //     .get("http://localhost:3001/notes")
+  //     .then((response) => {
+  //       console.log("promise fulfilled");
+  //       setNotes(response.data);
+  //     });
+  // }, []);
+  // console.log('render', notes.length, 'notes');
+
+  const hook = () => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled');
+        setNotes(response.data);
+      });
+  }
+
+  useEffect(hook, []) //?? run every completed render, but you can choose to fire it only when certain values have changed.
+  
 
   const addNote = (event) => {
     event.preventDefault();
@@ -37,7 +63,7 @@ const App = (props) => {
         </button>
       </div>
       <ul>
-        {notesToShow.map((note) => (
+        {notesToShow?.map((note) => (
           <Note key={note.id} note={note} />
         ))}
 
--- Part-2/apart2-intro/2C-GettingDataFromServer/src/main.jsx
+++ Part-2/apart2-intro/2C-GettingDataFromServer/src/main.jsx
@@ -23,32 +23,32 @@ import App from "./App.jsx";
 // const promise2 = axios.get("http://localhost:3001/foobar");
 // console.log(promise2);
 
-axios
-  .get('http://localhost:3001/notes')
-  .then(response => {
-    const notes = response.data
-    ReactDOM.createRoot(document.getElementById('root'))
-      .render(<App notes={notes} />)
-  })
+// axios
+//   .get('http://localhost:3001/notes')
+//   .then(response => {
+//     const notes = response.data
+//     ReactDOM.createRoot(document.getElementById('root'))
+//       .render(<App notes={notes} />)
+//   })
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+// const notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     important: true,
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only JavaScript",
+//     important: false,
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     important: true,
+//   },
+// ];
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <App notes={notes} />
-// );
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App  />
+);
